a48f4194d05d8c853a3fd7b60b393f798b8659bb
Thanakit Jitcharoenjai <smild_dream@hotmail.com>
2022-07-03 16:50:15 +0700

    initail commit tak_20220703

--- test1.py
+++ test1.py
@@ -97,3 +97,71 @@ def v6():
 # results = list(map(addition, num))
 # results = list(map(lambda x, y: x + y, num1, num2))
 # print(results)
+
+##############################################################################
+# ex1 #
+
+def add_more_two_dec(call_func): # เอา output ของ  call_func() มาใช้ คือ 1 
+    def add_two () :
+        return call_func() + 2
+    return add_two
+
+def add_more_dec(call_func): # เอา output ของ  call_func() มาใช้ คือ 1 
+    def add_one () :
+        return call_func() + 1
+    return add_one
+    
+@add_more_two_dec
+@add_more_dec
+def init_one():  # เอาไปใช้กับ function อะไรบ้างให้ เพิ่ม @ชื่อ function ที่ต้องการ output ไปใช้
+    return 1
+
+# print(init_one())
+
+# ex2 #
+def time_three_dec (call_func): # เอา ouput จาก step ที่ 1 มาคำนวณ
+    def mul_one (number) :
+        return call_func(number) * 9999
+    return mul_one
+
+def time_two_dec (call_func):  # ทำงาน step ที่ 1 
+    def time_two (number) :
+        return call_func(number) + number
+    return time_two
+
+@time_three_dec # ทำงาน step ที่ 2
+@time_two_dec # ทำงาน step ที่ 1 
+def init_number (number) :
+    print(number)
+    return number
+
+
+# print(init_number(9))
+
+
+# ex3 #
+
+# def greet_two(dec_arg1, dec_arg2):
+#     def decorator (call_func) : 
+#         def get_greeting_msg (func_arg1, func_arg2):
+#             print(f"call function step 1 =>, {func_arg1}, {func_arg2} from inside wrapper")   
+#             return f'call function step 2 => {call_func(dec_arg1, dec_arg2)}'
+#         return get_greeting_msg
+#     return decorator
+
+
+def greet_one (dec_arg1, dec_arg2) : # args -> poy, toon
+    def decorator (call_func) :  # call_func = greet_two_people
+        def get_greeting_msg (func_arg1, func_arg2) : # func_arg1 -> tak, func_arg2 -> tok
+            print("working on step => 2", func_arg1, func_arg2, dec_arg1, dec_arg2, " from inside wrapper")
+            return call_func(func_arg1, func_arg2)
+        return get_greeting_msg   
+    return decorator
+
+# @greet_two("poy", "toon")
+@greet_one("poy", "toon")
+def greet_two_people (name1, name2) :
+    print(f'working on step => 1, {name1}, {name2}, From Normal Function')
+    return name1, name2
+
+print(greet_two_people("tak", "tok"))
